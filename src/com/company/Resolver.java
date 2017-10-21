c26cce6bee6662d1c7f17ab5d12464fbe189c503
stefan-cosmin <steancosmin.romanescu@gmail.com>
2017-10-21 13:16:18 +0300

    Resolver now uses state history for random algorithm

--- src/com/company/Resolver.java
+++ src/com/company/Resolver.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class Resolver {
 
-    public void run(String methodName, int ... args) {
-        if(Objects.equals(methodName, "random")) {
+    public void run(String methodName, int... args) {
+        if (Objects.equals(methodName, "random")) {
             random(args[0], args[1]);
         }
     }
@@ -18,29 +18,47 @@ public class Resolver {
 
         int loopCounter = 0;
         int failedCounter = 0;
+        Random random = new Random();
         while (!hanoi.isFinalState(state)) {
-            Random random = new Random();
             int selectedPosition = random.nextInt(numberOfDisks);
             int selectedTower = random.nextInt(numberOfTowers);
             if (hanoi.isValidState(state, selectedPosition, selectedTower)) {
                 state.updateState(selectedPosition, selectedTower);
+                hanoi.addStateToHistory(state);
             }
             loopCounter++;
 
-            if(loopCounter > 1000 * numberOfTowers) {
+            if (loopCounter > 1000 * numberOfTowers) {
                 ++failedCounter;
                 System.out.println("Failted to find a solution: " + failedCounter);
                 state = hanoi.init(numberOfTowers, numberOfDisks);
                 loopCounter = 0;
             }
 
-            if(failedCounter >= 10000) {
+            if (failedCounter >= 10000) {
                 System.out.println("Failed to find any solution");
                 break;
             }
         }
 
-        System.out.println("Finished");
+        System.out.println("Finished in " + loopCounter);
         System.out.println(state);
+    }
+
+    public void BK(State state, Hanoi hanoi, int numberOfTowers, int numberOfDisks) {
+        if (hanoi.isFinalState(state)) {
+            System.out.println(state);
+        }
+        int[] nDisk = new int[numberOfDisks];
+        for (int i = 0; i < numberOfDisks; ++i) {
+            nDisk[i] = state.getElementAt(i);
+        }
+
+        for (int i : nDisk) {
+            for (int j = i; j < numberOfTowers; ++j) {
+
+            }
+
+        }
     }
 }
